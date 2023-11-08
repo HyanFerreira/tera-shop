b8541522ec93b8f3edd00469de4ee06234f4def5
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-11-08 11:18:02 -0300

    update carousel - 08/11

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -52,3 +52,63 @@ document.addEventListener('DOMContentLoaded', function () {
     intervalId = setInterval(autoAdvanceSlide, 5000);
   });
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var carouselSlide = document.querySelector('.carousel-slide');
+//   var prevBtn = document.getElementById('prevBtn');
+//   var nextBtn = document.getElementById('nextBtn');
+
+//   function updateBannerSlides() {
+//     return document.querySelectorAll('.banner-slide');
+//   }
+
+//   var currentSlide = 0;
+
+//   prevBtn.addEventListener('click', function () {
+//     currentSlide =
+//       (currentSlide - 1 + bannerSlides.length) % bannerSlides.length;
+//     updateSlidePosition();
+//   });
+
+//   nextBtn.addEventListener('click', function () {
+//     currentSlide = (currentSlide + 1) % bannerSlides.length;
+//     updateSlidePosition();
+//   });
+
+//   function updateSlidePosition() {
+//     carouselSlide.style.transform = `translateX(-${currentSlide * 25}%)`;
+//   }
+
+//   var bannerSlides = updateBannerSlides();
+//   updateSlidePosition();
+
+//   function autoAdvanceSlide() {
+//     nextBtn.click();
+//   }
+
+//   var intervalId = setInterval(autoAdvanceSlide, 5000);
+
+//   carouselSlide.addEventListener('mouseenter', function () {
+//     clearInterval(intervalId);
+//   });
+
+//   carouselSlide.addEventListener('mouseleave', function () {
+//     intervalId = setInterval(autoAdvanceSlide, 5000);
+//   });
+
+//   prevBtn.addEventListener('mouseenter', function () {
+//     clearInterval(intervalId);
+//   });
+
+//   prevBtn.addEventListener('mouseleave', function () {
+//     intervalId = setInterval(autoAdvanceSlide, 5000);
+//   });
+
+//   nextBtn.addEventListener('mouseenter', function () {
+//     clearInterval(intervalId);
+//   });
+
+//   nextBtn.addEventListener('mouseleave', function () {
+//     intervalId = setInterval(autoAdvanceSlide, 5000);
+//   });
+// });
