24297d1b6f713a9eb16f683a99d1b976dcc22658
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-11-11 20:21:00 -0300

    update carousel.js - 11/11

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const prevBtn = document.getElementById('prevBtn');
   const nextBtn = document.getElementById('nextBtn');
+  const btnStartPause = document.getElementById('btnStartPause');
+  const imgStartPause = document.getElementById('imgStartPause');
 
   let isTransitioning = false;
 
@@ -43,17 +45,21 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   function autoAdvanceSlide() {
-    nextBtn.click()
+    nextBtn.click();
   }
 
   var intervalId;
+  var isPaused = false;
 
   function startAutoAdvance() {
-    clearTimeout(intervalId); // Cancela o timeout anterior
-    intervalId = setTimeout(function () {
-      autoAdvanceSlide();
-      startAutoAdvance();
-    }, 5000);
+    clearTimeout(intervalId);
+
+    if (!isPaused) {
+      intervalId = setTimeout(function () {
+        autoAdvanceSlide();
+        startAutoAdvance();
+      }, 5000);
+    }
   }
 
   startAutoAdvance();
@@ -81,130 +87,15 @@ document.addEventListener('DOMContentLoaded', function () {
   nextBtn.addEventListener('mouseleave', function () {
     startAutoAdvance();
   });
+
+  btnStartPause.addEventListener('click', function () {
+    isPaused = !isPaused;
+    btnStartPause.classList.toggle('paused', isPaused);
+    imgStartPause.src = './assets/img/play_circle.svg';
+
+    if (!isPaused) {
+      imgStartPause.src = './assets/img/pause_circle.svg';
+      startAutoAdvance();
+    }
+  });
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var carouselSlide = document.querySelector('.carousel-slide');
-//   var prevBtn = document.getElementById('prevBtn');
-//   var nextBtn = document.getElementById('nextBtn');
-
-//   function updateBannerSlides() {
-//     return document.querySelectorAll('.banner-slide');
-//   }
-
-//   prevBtn.addEventListener('click', function () {
-//     carouselSlide.insertBefore(
-//       bannerSlides[bannerSlides.length - 1],
-//       bannerSlides[0],
-//     );
-//     bannerSlides = updateBannerSlides();
-//   });
-
-//   nextBtn.addEventListener('click', function () {
-//     carouselSlide.appendChild(bannerSlides[0]);
-//     bannerSlides = updateBannerSlides();
-//   });
-
-//   var bannerSlides = updateBannerSlides();
-
-//   function autoAdvanceSlide() {
-//     nextBtn.click();
-//   }
-
-//   var intervalId;
-
-//   function startAutoAdvance() {
-//     clearTimeout(intervalId); // Cancela o timeout anterior
-//     intervalId = setTimeout(function () {
-//       autoAdvanceSlide();
-//       startAutoAdvance();
-//     }, 5000);
-//   }
-
-//   startAutoAdvance();
-
-//   carouselSlide.addEventListener('mouseenter', function () {
-//     clearTimeout(intervalId);
-//   });
-
-//   carouselSlide.addEventListener('mouseleave', function () {
-//     startAutoAdvance();
-//   });
-
-//   prevBtn.addEventListener('mouseenter', function () {
-//     clearTimeout(intervalId);
-//   });
-
-//   prevBtn.addEventListener('mouseleave', function () {
-//     startAutoAdvance();
-//   });
-
-//   nextBtn.addEventListener('mouseenter', function () {
-//     clearTimeout(intervalId);
-//   });
-
-//   nextBtn.addEventListener('mouseleave', function () {
-//     startAutoAdvance();
-//   });
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var carouselSlide = document.querySelector('.carousel-slide');
-//   var prevBtn = document.getElementById('prevBtn');
-//   var nextBtn = document.getElementById('nextBtn');
-
-//   function updateBannerSlides() {
-//     return document.querySelectorAll('.banner-slide');
-//   }
-
-//   var currentSlide = 0;
-
-//   prevBtn.addEventListener('click', function () {
-//     currentSlide =
-//       (currentSlide - 1 + bannerSlides.length) % bannerSlides.length;
-//     updateSlidePosition();
-//   });
-
-//   nextBtn.addEventListener('click', function () {
-//     currentSlide = (currentSlide + 1) % bannerSlides.length;
-//     updateSlidePosition();
-//   });
-
-//   function updateSlidePosition() {
-//     carouselSlide.style.transform = `translateX(-${currentSlide * 25}%)`;
-//   }
-
-//   var bannerSlides = updateBannerSlides();
-//   updateSlidePosition();
-
-//   function autoAdvanceSlide() {
-//     nextBtn.click();
-//   }
-
-//   var intervalId = setInterval(autoAdvanceSlide, 5000);
-
-//   carouselSlide.addEventListener('mouseenter', function () {
-//     clearInterval(intervalId);
-//   });
-
-//   carouselSlide.addEventListener('mouseleave', function () {
-//     intervalId = setInterval(autoAdvanceSlide, 5000);
-//   });
-
-//   prevBtn.addEventListener('mouseenter', function () {
-//     clearInterval(intervalId);
-//   });
-
-//   prevBtn.addEventListener('mouseleave', function () {
-//     intervalId = setInterval(autoAdvanceSlide, 5000);
-//   });
-
-//   nextBtn.addEventListener('mouseenter', function () {
-//     clearInterval(intervalId);
-//   });
-
-//   nextBtn.addEventListener('mouseleave', function () {
-//     intervalId = setInterval(autoAdvanceSlide, 5000);
-//   });
-// });
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-const hamburguer = document.querySelector('.hamburguer');
-const asideNavigation = document.querySelector('.aside-navigation');
-const overlow = document.querySelector('.overlow');
-const bodyOverflow = document.querySelector('body');
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburguer = document.querySelector('.hamburguer');
+  const asideNavigation = document.querySelector('.aside-navigation');
+  const overlow = document.querySelector('.overlow');
+  const bodyOverflow = document.querySelector('body');
 
-hamburguer.addEventListener('click', () => {
-  overlow.classList.toggle('active');
-  bodyOverflow.classList.toggle('active');
-  asideNavigation.classList.toggle('active');
+  hamburguer.addEventListener('click', () => {
+    overlow.classList.toggle('active');
+    bodyOverflow.classList.toggle('active');
+    asideNavigation.classList.toggle('active');
+  });
+
+  overlow.addEventListener('click', () => {
+    overlow.classList.toggle('active');
+    bodyOverflow.classList.toggle('active');
+    asideNavigation.classList.toggle('active');
+  });
 });
-
-overlow.addEventListener('click', () => {
-  overlow.classList.toggle('active');
-  bodyOverflow.classList.toggle('active');
-  asideNavigation.classList.toggle('active');
-});
-
