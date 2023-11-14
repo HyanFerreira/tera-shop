57cc8d73ff94e661f4a30dc936d5e42c9cc16bac
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-11-14 11:27:02 -0300

    update index.html - 14/11

---
+++ assets/js/carousel-products.js
@@ -0,0 +1,17 @@
+// const productContainers = [...document.querySelectorAll('.product-container')];
+// const prevBtnProducts = [...document.querySelectorAll('.prevBtnProducts')];
+// const nextBtnProducts = [...document.querySelectorAll('.nextBtnProducts')];
+
+// productContainers.forEach((item, i) => {
+//   let containerDimenstions = item.getBoundingClientRect();
+//   let containerWidth = containerDimenstions.width;
+
+//   nextBtnProducts[i].addEventListener('click', () => {
+//     item.scrollLeft += containerWidth;
+//   });
+
+//   prevBtnProducts[i].addEventListener('click', () => {
+//     item.scrollLeft -= containerWidth;
+//   });
+// });
+
